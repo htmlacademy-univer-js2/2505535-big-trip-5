4c88fbd3a93a8e278361ddae2b5de737e2c4c539
platunova-ann <aplatunovasergeeva@gmail.com>
2025-05-11 23:30:08 +0500

    удалила

--- src/view/EditFormView.js
+++
@@ -1,7 +0,0 @@
-export default class EditFormView {
-  getElement() {
-    const element = document.createElement('div');
-    element.innerHTML = '<form class="edit-form">Форма редактирования</form>';
-    return element;
-  }
-}
