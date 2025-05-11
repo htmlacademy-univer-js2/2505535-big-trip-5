0be3f3ec345d9724f146f2697d24534902b88ae6
platunova-ann <aplatunovasergeeva@gmail.com>
2025-05-11 21:41:25 +0500

    11.05.2025

--- bundle.js
+++ bundle.js
@@ -1,5 +1,14 @@
-
-
-{
-  "presets": ["@babel/preset-env"]
-}
+ (() => { // webpackBootstrap
+ 	var __webpack_modules__ = ({
+		"./src/main.js": (() => {
+		// Ваш исходный код
+			console.log("Hello, world!");
+		}),
+	"./node_modules/dayjs/dayjs.min.js": (() => {
+		// Код библиотеки dayjs
+	}),
+	});
+	// Служебный код Webpack
+ 	var __webpack_exports__ = {};
+ 	__webpack_modules__["./src/main.js"]();
+ })();
